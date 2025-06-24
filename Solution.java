4bec59f03df97d29c92bcc7d7ba790a27de19955
Chaitanya Datar <datar.chaitanya@gmail.com>
2025-06-24 17:55:39 -0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(0);
+
+        for(int i = 0; i < operations.length; i++){
+            if(operations[i].equals("D")){
+                list.add(list.get(list.size() - 1) * 2);
+            }
+            else if(operations[i].equals("C")){
+                list.remove(list.size() - 1);
+            }
+            else if(operations[i].equals("+")){
+                list.add(list.get((list.size() - 1)) + list.get((list.size() - 2)));
+            }
+            else{
+                list.add(Integer.parseInt(operations[i]));
+            }
+        }
+
+        int sum = 0;
+        for(int i = 0; i < list.size(); i++){
+            sum = sum + list.get(i);
+        }
+
+        return(sum);
+    }
+}
